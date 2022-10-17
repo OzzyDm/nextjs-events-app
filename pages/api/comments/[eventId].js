51e8e61dc2dfb6c8ec648cdaf152f4230cf44c53
unknown <oguzhandemiraslan@gmail.com>
2022-10-17 14:46:21 -0400

    comments connected to db

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,9 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect(process.env.MONGODB);
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -16,23 +20,32 @@ function handler(req, res) {
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
 
-    console.log(newComment);
+    const db = client.db();
+
+    const result = await db.collection("comments").insertOne(newComment);
+
+    console.log(result);
     res.status(201).json({ message: "Added commment.", comment: newComment });
   }
   if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Ozzy", text: "First comment" },
-      { id: "c2", name: "Ozi", text: "Second comment" },
-    ];
+    const db = client.db();
 
-    res.status(200).json({ comments: dummyList });
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    res.status(200).json({ comments: documents });
   }
+
+  client.close();
 }
 
 export default handler;
