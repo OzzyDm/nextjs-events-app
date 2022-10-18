92f4a6364799b99e9821f1798d090d39ceaf5767
unknown <oguzhandemiraslan@gmail.com>
2022-10-18 15:12:28 -0400

    error handling added

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(process.env.MONGODB);
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection("emails").insertOne({ email: userEmail });
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,12 +21,21 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(process.env.MONGODB);
-    const db = client.db();
+    let client;
 
-    await db.collection("emails").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed." });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed." });
+    }
 
     res.status(201).json({ message: "Signed up!" });
   }
